b8f0331214f1ca8029d8abac06827ed6c1918a28
Mominis <46275902+kkrnme@users.noreply.github.com>
2020-04-21 23:27:35 +0900

    add media queries

---
+++ src/styles/mediaQueries.ts
@@ -0,0 +1,12 @@
+/**
+ * `sm` : 640px~
+ * `md` : 768px~
+ * `lg` : 1024px~
+ * `xl` : 1280px~
+ */
+export const mq: { [index in "sm" | "md" | "lg" | "xl"]: string } = {
+  sm: `@media (min-width: 640px)`,
+  md: `@media (min-width: 768px)`,
+  lg: `@media (min-width: 1024px)`,
+  xl: `@media (min-width: 1280px)`,
+}
