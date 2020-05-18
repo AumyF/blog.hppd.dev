05356bf9065d6036c73ff1053206569d86816568
Mominis <46275902+kkrnme@users.noreply.github.com>
2020-05-18 19:22:20 +0900

    aタグネストを解消

--- src/components/tag-list/tag-list.tsx
+++ src/components/tag-list/tag-list.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { css } from "@emotion/core";
 import { faTags } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { styleValues } from "../../styles/styleValues";
 
 export type TagListProps = {
   tags: readonly string[];
@@ -21,19 +22,14 @@ export const TagList: React.FC<TagListProps> = ({ tags }) => (
     >
       {tags?.map(tag => (
         <li
+          key={tag}
           css={css`
             font-size: 1em;
             margin-left: 0.3rem;
+            color: ${styleValues.global.primaryAccent};
           `}
         >
-          <a
-            css={css`
-              text-decoration: none;
-            `}
-            href={`/tags/${tag}/`}
-          >
-            {tag}
-          </a>
+          {tag}
         </li>
       ))}
     </ul>
