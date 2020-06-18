c30f622fe552aee9de5d8193780bc55f00577b72
Mominis <46275902+kkrnme@users.noreply.github.com>
2020-06-19 01:26:41 +0900

    typesafe selectつくってみた

--- src/components/sidebar/typesafe-select.tsx
+++ src/components/sidebar/typesafe-select.tsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { DOMAttributes, HTMLAttributes } from "react";
+
+type htmlSelect = HTMLAttributes<HTMLSelectElement>;
 
 export type SelectProps<T extends string[]> = {
   options: T;
+  onChangeHandler: (arg: ElementOf<T>) => unknown;
+  className?: string;
+  defaultValue: htmlSelect["defaultValue"];
 };
 
-export const Select = <T extends string[]>({ options }: SelectProps<T>) => {
+type ElementOf<A extends unknown[]> = A extends (infer R)[] ? R : never;
+
+export const Select = <T extends string[]>({
+  options,
+  onChangeHandler,
+  className,
+  defaultValue,
+}: SelectProps<T>) => {
+  const onChange: htmlSelect["onSelect"] = e =>
+    onChangeHandler(e.currentTarget.value as ElementOf<T>);
   return (
-    <select name="" id="">
+    <select {...{ onChange, className, defaultValue }}>
       {options.map(opt => (
-        <option value={opt}>{opt}</option>
+        <option value={opt} key={opt}>
+          {opt}
+        </option>
       ))}
     </select>
   );
