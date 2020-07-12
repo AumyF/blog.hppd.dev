697d0c19ffce6cc746de7fe237dbb5726084ea6d
Mominis <46275902+kkrnme@users.noreply.github.com>
2020-07-12 17:52:34 +0900

    記事の位置変更

--- src/gatsby/gatsby-config.ts
+++ src/gatsby/gatsby-config.ts
@@ -25,7 +25,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `posts`,
-        path: `${__dirname}/../posts`,
+        path: `${__dirname}/../../posts`,
       },
     },
     {
