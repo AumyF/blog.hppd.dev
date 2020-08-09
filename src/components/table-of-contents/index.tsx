f64ddd743d2eb1a6f16e7ad1f0ab9cfb6efe72ce
Aumy <46275902+AumyF@users.noreply.github.com>
2020-08-09 20:38:00 +0900

    tocのスタイル調整

--- src/components/table-of-contents/index.tsx
+++ src/components/table-of-contents/index.tsx
@@ -1,47 +1,61 @@
 import React from "react";
 import { css } from "@emotion/core";
 import { TOC } from "../../libs/toc";
-import styled from "@emotion/styled";
+import { withTheme } from "../../styles/theme";
+import tw from "twin.macro";
 
-const TOCItemComponent: React.FCX<TOC> = ({ items, className }) => (
+const ItemBase: React.FCX<TOC> = ({ items, className }) => (
   <ul className={className}>
     {items.map(items => (
       <li key={items.title} className="leading-normal" css={css``}>
         <a
-          className="block my-1 no-underline transition-all duration-75 hover:bg-border"
+          className="block my-1 no-underline transition-all duration-75"
           href={items.url}
         >
           {items.title}
         </a>
-        {items.items ? (
-          <TOCItemComponent className="ml-4" items={items.items} />
-        ) : null}
+        {items.items ? <Item className="ml-4" items={items.items} /> : null}
       </li>
     ))}
   </ul>
 );
 
+const Item = withTheme(ItemBase, {
+  dark: css`
+    > li > a {
+      ${tw`hover:bg-gray-800`}
+    }
+  `,
+  light: css`
+    > li > a {
+      ${tw`hover:bg-gray-300`}
+    }
+  `,
+});
+
 export type TableOfContentsProps = {
   toc: TOC;
 };
 
-const PlainComponent: React.FCX<TableOfContentsProps> = ({
+const TableOfContentsBase: React.FCX<TableOfContentsProps> = ({
   toc: TOC,
   className,
 }) => (
-  <div className={className}>
-    <div>Table of Contents</div>
-    <TOCItemComponent items={TOC.items} />
+  <div className={className + " px-4 py-2"}>
+    <div className="text-center pb-2 px-4 border-b">Table of Contents</div>
+    <Item className="pt-2" items={TOC.items} />
   </div>
 );
 
-export const TableOfContents = styled(PlainComponent)`
-  font-size: 1rem;
-  padding: 0.5rem 1rem;
-  & > div {
-    text-align: center;
-    margin: auto 1rem;
-    padding-bottom: 0.3rem;
-    border-bottom: 1px solid var(--border);
-  }
-`;
+export const TableOfContents = withTheme(TableOfContentsBase, {
+  dark: css`
+    > div {
+      ${tw`border-gray-700`}
+    }
+  `,
+  light: css`
+    > div {
+      ${tw`border-gray-200`}
+    }
+  `,
+});
