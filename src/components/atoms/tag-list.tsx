609aa1e5dbf5546e20a4c9f6cb48cbe96fe2370a
Aumy <46275902+AumyF@users.noreply.github.com>
2020-08-12 01:10:33 +0900

    tag-list の間隔を変える

--- src/components/atoms/tag-list.tsx
+++ src/components/atoms/tag-list.tsx
@@ -7,12 +7,13 @@ import { Link } from "gatsby";
 export type TagListProps = Pick<Post, "tags">;
 
 const Tag: React.FCX<{ tag: Post["tags"][number] }> = ({ tag, className }) => (
-  <Link className={"text-teal-500 px-1 " + className} to={`/tags/${tag}`}>
+  <Link className={"text-teal-500 " + className} to={`/tags/${tag}`}>
     {tag}
   </Link>
 );
+
 export const TagList: React.FCX<TagListProps> = ({ tags, className }) => (
-  <div className={className + " inline-flex flex-wrap"}>
+  <div className={className + " inline-flex flex-wrap gap-1"}>
     <span>
       <FontAwesomeIcon icon={faTags} />
     </span>
