bae590766521950f716ac8bdcab5ba1a93a6f74f
Aumy <46275902+AumyF@users.noreply.github.com>
2020-08-13 01:55:25 +0900

    articleの余計なのを削除

--- src/components/article/index.tsx
+++ src/components/article/index.tsx
@@ -1,13 +1,6 @@
-import React from "react";
 import styled from "@emotion/styled";
 import { ArticleStyles } from "./article-styles";
 
-type ArticleProps = {};
-
-const ArticleBase: React.FCX<ArticleProps> = ({ children, className }) => (
-  <article {...{ children, className }} />
-);
-
-export const Article = styled(ArticleBase)`
+export const Article = styled.article`
   ${ArticleStyles};
 `;
