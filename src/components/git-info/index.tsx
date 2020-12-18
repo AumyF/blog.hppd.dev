318ebddd44fb376d6a444ef7af9033fbea7ae706
Aumy <46275902+AumyF@users.noreply.github.com>
2020-12-18 17:43:41 +0000

    git info変更

--- src/components/git-info/index.tsx
+++ src/components/git-info/index.tsx
@@ -1,5 +1,14 @@
-import { Table, Tbody, Td, Text, Th, Tr, VStack } from "@chakra-ui/react";
-import React from "react";
+import {
+  Skeleton,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Tr,
+  VStack,
+} from "@chakra-ui/react";
+import React, { FC } from "react";
 
 import { Hyper } from "../atoms/Hyper";
 import { useLatestCommit } from "./useLatestCommit";
@@ -15,47 +24,62 @@ type PlainProps =
       status: "Success";
       url: string;
     }
-  | { status: "Loading" };
+  | {
+      lastUpdate?: string;
+      message?: string;
+      path?: string;
+      status: "Loading";
+      url?: string;
+    };
 
-const Plain: React.FC<PlainProps> = props => (
-  <VStack spacing=".5rem">
-    {props.status === "Error" ? (
-      <Text>{props.errMessage}</Text>
-    ) : props.status === "Loading" ? (
-      <Text>Loading...</Text>
-    ) : (
-      <>
-        <Table className="my-2" size="sm">
-          <Tbody>
-            <Tr>
-              <Th>Date</Th>
-              <Td>{props.lastUpdate}</Td>
-            </Tr>
-            <Tr>
-              <Th>Message</Th>
-              <Td>{props.message}</Td>
-            </Tr>
-            <Tr>
-              <Th>Hash</Th>
-              <Td>
-                <Hyper to={props.url}>
-                  {props.url.split("/").pop()?.substr(0, 7)}
-                </Hyper>
-              </Td>
-            </Tr>
-          </Tbody>
-        </Table>
-        <Hyper
-          textAlign="center"
-          display="block"
-          to={`https://github.com/AumyF/blog/blob/master/posts/${props.path}.mdx`}
-        >
-          View source on GitHub
-        </Hyper>
-      </>
-    )}
-  </VStack>
-);
+const Plain: React.FC<PlainProps> = props => {
+  if (props.status === "Error") return <Text>{props.errMessage}</Text>;
+  return (
+    <VStack spacing=".5rem">
+      {
+        <>
+          <Table size="sm">
+            <Tbody>
+              <Tr>
+                <Th>Date</Th>
+                <Td>
+                  <Skeleton isLoaded={props.status === "Success"}>
+                    {props.lastUpdate}
+                  </Skeleton>
+                </Td>
+              </Tr>
+              <Tr>
+                <Th>Message</Th>
+                <Td>
+                  <Skeleton isLoaded={props.status === "Success"}>
+                    {props.message}
+                  </Skeleton>
+                </Td>
+              </Tr>
+              <Tr>
+                <Th>Hash</Th>
+                <Td>
+                  <Skeleton isLoaded={props.status === "Success"}>
+                    <Hyper to={props.url ?? ""}>
+                      {props.url?.split("/").pop()?.substr(0, 7)}
+                    </Hyper>
+                  </Skeleton>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+          <Hyper
+            textAlign="center"
+            display="block"
+            to={`https://github.com/AumyF/blog/blob/master/posts/${props.path}.mdx`}
+          >
+            View source on GitHub
+          </Hyper>
+        </>
+      }
+    </VStack>
+  );
+};
 
 export const GitInfo: React.FC<GitInfoProps> = ({ filePath: path }) => {
   path = `${path}/index`;
